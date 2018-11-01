1bbd1ecf411dc8d46dd1154341a910209ee07842
cani-ic <cani_ic@163.com>
2018-11-01 22:55:59 +0800

    add interact() method

--- PP4E/System/func.py
+++ PP4E/System/func.py
@@ -8,3 +8,16 @@ def getopts(argv):
         else:
             argv=argv[1:]
     return opts
+
+
+def interact():
+    print('hello stream world')
+    while True:
+        try:
+            reply = input('Enter a Number >> ')
+      # except: # must specify exception name !!!
+        except EOFError: 
+            break
+        else:
+            num = int(reply)
+        print("%d squared is %d" % (num,num**2) ) # pay attention to the third "%" in this line !!!
--- PP4E/System/testargv.py
+++ PP4E/System/testargv.py
@@ -1,7 +1,8 @@
 import sys
-from func import getopts
+from func import getopts,interact
 myargs = getopts(sys.argv)
 if '-i' in myargs:
     print(myargs['-i'])
 print(myargs)
 
+interact()
